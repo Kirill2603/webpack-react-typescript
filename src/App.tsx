8bcfb732431dd1f551dd69c59681e12d4307246b
kirill2603 <k.radobolsky@gmail.com>
2022-07-27 10:32:32 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  const unused = "something";
+
+  return (
+    <div className='component a'>
+        Hello Webpack!{unused}
+    </div>
+  )
+}
+
+export default App
